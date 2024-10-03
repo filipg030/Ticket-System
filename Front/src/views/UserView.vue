01228e9caeacf08fd82a61895da971fa78c0a909
Filip Grudziecki <fgrudziecki@ST2003.room514.zsl>
2024-10-03 17:21:57 +0200

    Changed colors

--- Front/src/views/UserView.vue
+++ Front/src/views/UserView.vue
@@ -9,6 +9,7 @@ export default {
       nazwisko:null,
       sala:null,
       problem:null,
+      pietro: null,
     }
   },
   components: {
@@ -35,26 +36,44 @@ export default {
     <div class="m-4 w-2/3 flex flex-col gap-1">
       <div class="flex flex-row justify-between gap-2">
         <div class="w-1/2 flex flex-col">
-          <label for="salaInput">imie</label>
-          <input class="seagullTextInput w-full" type="text" id="imieInput">
+          <label for="salaInput">Imie</label>
+          <input class="slateTextInput w-full" type="text" id="imieInput">
         </div>
 
         <div class="w-1/2 flex flex-col">
-          <label for="salaInput">nazwisko</label>
-          <input class="seagullTextInput w-full" type="text" id="nazwiskoInput">
+          <label for="salaInput">Nazwisko</label>
+          <input class="slateTextInput w-full" type="text" id="nazwiskoInput">
         </div>
       </div>
 
-      <label for="salaInput">sala</label>
+      <label for="pietroInput">Piętro</label>
       <div class="rounded-xl p-0 overflow-hidden flex flex-row">
-        <input class="w-full bg-seagull-500 p-2 text-slate-100 font-semibold focus:outline-none" type="text" id="salaInput">
-        <button class="cursor-pointer aspect-square w-8 bg-seagull-700 hover:bg-seagull-900 text-seagull-100"><i class="uil uil-angle-down"></i></button>
+        <select v-model="pietro" name="pietro" class="w-full bg-slate-600 p-2 text-slate-100 font-semibold focus:outline-none" id="pietroInput">
+          <option value="szatnie">Szatnie</option>
+          <option value="parter">Parter</option>
+          <option value="1">Piętro 1</option>
+          <option value="2">Piętro 2</option>
+          <option value="pracParter">Pracownia parter</option>
+          <option value="prac1">Pracownia piętro 1</option>
+          <option value="int2">Internat piętro 2</option>
+          
+        </select>
       </div>
 
-      <label for="problemInput">problem</label>
-      <textarea class="seagullTextArea" id="problemInput" rows="10"></textarea>
 
-      <label for="problemInput">powaga</label>
+      <label for="salaInput">Sala</label>
+      <div class="rounded-xl p-0 overflow-hidden flex flex-row">
+        <select v-model="sala" name="sala" class="w-full bg-slate-600 p-2 text-slate-100 font-semibold focus:outline-none" id="salaInput">
+
+        </select>
+      </div>
+
+
+
+      <label for="problemInput">Problem</label>
+      <textarea class="slateTextArea" id="problemInput" rows="10"></textarea>
+
+      <label for="problemInput">Powaga</label>
       <div class="flex flex-row gap-2">
         <button class="pointer p-2 rounded-md bg-red-500 hover:bg-red-600 w-full"
           :class="{ 'inactiveBtn': severity != 2 }" @click="severity = 2">Ważne!</button>
