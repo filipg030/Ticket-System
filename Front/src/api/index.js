ee7095e791eaaf14ccb4eaef740868c4cc11f4cc
Filip Grudziecki <fgrudziecki@ST2003.room514.zsl>
2024-10-03 14:47:25 +0200

    axios

---
+++ Front/src/api/index.js
@@ -0,0 +1,71 @@
+import axios from "axios"
+
+const get = async (url) => {
+
+
+        setTimeout(async () => {
+            try {
+                const response = await axios.get(url)
+                resolve(response.data)
+            } catch (err) {
+                reject(err)
+            }
+
+
+    })
+}
+
+
+
+const getAuth = async (url, token) => {
+
+        if (token == null) {
+            resolve("Not logged in")
+        }
+        setTimeout(async () => {
+            try {
+                const response = await axios.get(url, { headers: { 'Authorization': `Bearer ${token}` } })
+                resolve(response.data)
+            } catch (err) {
+                reject(err)
+            }
+
+
+    })
+}
+
+
+const post = async (url, object) => {
+
+    return new Promise(async (resolve, reject) => {
+
+            try {
+                const response = await axios.post(url, object)
+                resolve(response.data)
+            } catch (err) {
+                reject(err)
+            }
+
+
+
+    })
+}
+const patch = async (url, object) => {
+
+    return new Promise(async (resolve, reject) => {
+
+            try {
+                const response = await axios.patch(url, object)
+                resolve(response.data)
+            } catch (err) {
+                reject(err)
+            }
+
+    })
+}
+
+
+
+export {
+
+}
